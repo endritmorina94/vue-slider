d572c94b0cf7976dd31d4937990ab6b95811a757
endritmorina94 <endrit.morina@outlook.it>
2021-04-21 13:50:50 +0200

    aggiunto commento con la consegna dell'esercizio

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+//CONSEGNA:
+//Rifare l'esercizio dello slider come fatto assieme in classe.
+//Bonus:
+//Applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente.
+
 var root = new Vue({
 
     el: "#root",
